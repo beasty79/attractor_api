cffb6b5d4082e11e04d637e5e20e8fd3e5eeb48e
Silas-desktop <silasfelix2005@gmail.com>
2025-08-03 15:57:26 +0200

    init

---
+++ attractor/__init__.py
@@ -0,0 +1,19 @@
+from .api import (
+    ColorMap,
+    Performance_Renderer,
+    sinspace,
+    cosspace,
+    bpmspace,
+    map_area,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "ColorMap",
+    "Performance_Renderer",
+    "sinspace",
+    "cosspace",
+    "bpmspace",
+    "map_area",
+]
