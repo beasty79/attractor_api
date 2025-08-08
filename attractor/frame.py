067979f0ced33306875629a53aa2d4b0262c10c8
Silas-desktop <silasfelix2005@gmail.com>
2025-08-08 23:57:34 +0200

    mess

--- attractor/frame.py
+++ attractor/frame.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from numpy.typing import NDArray
-from typing import Optional
+from typing import Optional, Iterable
 from .api import apply_color
 import numpy as np
 
@@ -11,16 +11,22 @@ class Frame:
     This Class Represents one render instance
     Identifiers like, a, b, c, ... are initial value for the strange attractors
     """
-    resolution: int
-    percentile: float
-    colors: NDArray
-    n: int
+    resolution: int | list[int]
+    percentile: float | list[float]
+    colors: NDArray | list[NDArray]
+    n: int | list[int]
 
     def __post_init__(self):
         # attributes only available after render
         self.img_: Optional[NDArray] = None
         self.raw_: Optional[NDArray] = None
         self.collapsed: bool = False
+
+    def set_static(self, resolution: bool, percentile: bool, colors: bool, n: bool) -> None:
+        return
+
+    def __len__(self) -> int:
+        return -1
 
     @property
     def img(self) -> NDArray:
@@ -30,8 +36,19 @@ class Frame:
         if self.raw is None:
             raise RuntimeError("Frame isn't rendered yet!")
 
+        self.check_multiple()
+        # self.colors: NDArray
+        if isinstance(self.colors, list):
+            raise Exception()
         self.img_ = apply_color(self.raw, self.colors)
         return self.img_
+
+    def check_multiple(self):
+        assert not isinstance(self.resolution, (list, np.ndarray))
+        assert not isinstance(self.n, (list, np.ndarray))
+        assert not isinstance(self.percentile, (list, np.ndarray))
+        assert not isinstance(self.colors[0][0], (list, np.ndarray))
+        return True
 
     @img.setter
     def img(self, value: NDArray):
@@ -47,7 +64,10 @@ class Frame:
         self.is_collapsed()
 
     def render(self, only_raw = False):
+        self.check_multiple()
         if not only_raw and self.raw is not None:
+            if isinstance(self.colors, list):
+                raise Exception()
             self.img = apply_color(self.raw, self.colors)
 
     def scatter_to_normalized(self, x_raw, y_raw):
@@ -64,22 +84,53 @@ class Frame:
 
     def is_collapsed(self):
         assert self.raw is not None, "first render"
+        assert not isinstance(self.resolution, list)
         non_zero = np.count_nonzero(self.raw)
         thresh = self.resolution ** 2 * 0.05
         self.collapsed = non_zero < thresh
 
+# resolution: int
+# percentile: float
+# colors: NDArray
+# n: int
 
 @dataclass
 class SimonFrame(Frame):
-    a: float
-    b: float
+    a: float | list
+    b: float | list
 
     def render(self, only_raw = False):
         from .attractor import simon
 
+        if isinstance(self.a, list) or isinstance(self.b, list):
+            raise ValueError("a or b are a list, when using lists for assignment call .toFrames() first")
+
         x, y = simon(self.a, self.b, self.n)
         self.raw = self.scatter_to_normalized(x, y)
         super().render(only_raw=only_raw)
+
+    def set_static(self, resolution: bool, percentile: bool, colors: bool, n: bool) -> None:
+        if self.resolution:
+            # self.resolution = [self.resolution] * len(self)
+
+
+
+
+    def toFrames(self) -> list[Frame]:
+
+        return []
+
+    def __len__(self) -> int:
+        if isinstance(self.a, list):
+            return len(self.a)
+        if isinstance(self.b, list):
+            return len(self.b)
+        if isinstance(self.n, list):
+            return len(self.n)
+        if isinstance(self.percentile, list):
+            return len(self.percentile)
+        return 1
+
 
 
 
@@ -96,6 +147,7 @@ class CliffordFrame(Frame):
     def render(self, only_raw = False):
         from .attractor import clifford
 
+        self.check_multiple()
         x, y = clifford(self.a, self.b, self.c, self.d, self.n)
         self.raw = self.scatter_to_normalized(x, y)
         super().render(only_raw=only_raw)
